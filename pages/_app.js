c08da9d84e4850a853032f3ab3178508a380f1d0
Brett Peterson <brett.peterson@getweave.com>
2021-08-17 09:13:14 -0600

    ampure

--- pages/_app.js
+++ pages/_app.js
@@ -15,36 +15,36 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "next/app";
-import Head from "next/head";
-import Router from "next/router";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'next/app';
+import Head from 'next/head';
+import Router from 'next/router';
 
-import PageChange from "components/PageChange/PageChange.js";
+import PageChange from 'components/PageChange/PageChange.js';
 
-import "assets/css/nextjs-material-dashboard.css?v=1.1.0";
+import 'assets/css/nextjs-material-dashboard.css?v=1.1.0';
 
-Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
-  );
+Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`);
+    document.body.classList.add('body-page-transition');
+    ReactDOM.render(
+        <PageChange path={url} />,
+        document.getElementById('page-transition')
+    );
 });
-Router.events.on("routeChangeComplete", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
+Router.events.on('routeChangeComplete', () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('page-transition'));
+    document.body.classList.remove('body-page-transition');
 });
-Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
+Router.events.on('routeChangeError', () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('page-transition'));
+    document.body.classList.remove('body-page-transition');
 });
 
 export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment(`
+    componentDidMount() {
+        let comment = document.createComment(`
 
 =========================================================
 * * NextJS Material Dashboard v1.1.0 based on Material Dashboard React v1.9.0
@@ -61,36 +61,36 @@ export default class MyApp extends App {
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 `);
-    document.insertBefore(comment, document.documentElement);
-  }
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+        document.insertBefore(comment, document.documentElement);
     }
+    static async getInitialProps({ Component, router, ctx }) {
+        let pageProps = {};
 
-    return { pageProps };
-  }
-  render() {
-    const { Component, pageProps } = this.props;
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
 
-    const Layout = Component.layout || (({ children }) => <>{children}</>);
+        return { pageProps };
+    }
+    render() {
+        const { Component, pageProps } = this.props;
 
-    return (
-      <React.Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>NextJS Material Dashboard by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </React.Fragment>
-    );
-  }
+        const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+        return (
+            <React.Fragment>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <title>Ampure. Pure SEO for dentists</title>
+                    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+                </Head>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </React.Fragment>
+        );
+    }
 }
