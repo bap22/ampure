b8a9dd92c72d613ac4a7bce2520929dbc20da321
Brett Peterson <brett.peterson@getweave.com>
2021-08-30 08:07:45 -0600

    starter theme

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const webpack = require("webpack");
-const path = require("path");
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
-});
+const nextConfiguration = {
+  target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+};
+
+module.exports = withPlugins([optimizedImages], nextConfiguration);
