2b00819c1233493db075497fb84d202468025ea1
Brett Peterson <brett.peterson@getweave.com>
2021-08-31 08:51:37 -0600

    theme ui updates

--- src/sections/feature.js
+++ src/sections/feature.js
@@ -1,3 +1,4 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Container, Grid } from 'theme-ui';
@@ -11,102 +12,96 @@ import Design from 'assets/feature/design.svg';
 import Chat from 'assets/feature/chat.svg';
 
 const data = [
-  {
-    id: 1,
-    imgSrc: Smart,
-    altText: 'Smart',
-    title: 'Smart Features',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
-  {
-    id: 2,
-    imgSrc: Winner,
-    altText: 'Performance',
-    title: 'Fast Performance',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
-  {
-    id: 3,
-    imgSrc: Cloud,
-    altText: 'Content',
-    title: 'Unlimited Content',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
-  {
-    id: 4,
-    imgSrc: Setting,
-    altText: 'Customization',
-    title: 'Unlimited Customization',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
-  {
-    id: 5,
-    imgSrc: Design,
-    altText: 'Productivity',
-    title: 'Boost Productivity',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
-  {
-    id: 6,
-    imgSrc: Chat,
-    altText: 'Support',
-    title: 'Customer Support',
-    text:
-      'Get your blood tests delivered at let home collect sample from the victory of the managements.',
-  },
+    {
+        id: 1,
+        imgSrc: Smart,
+        altText: 'Smart',
+        title: 'Smart Features',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: 2,
+        imgSrc: Winner,
+        altText: 'Performance',
+        title: 'Fast Performance',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: 3,
+        imgSrc: Cloud,
+        altText: 'Content',
+        title: 'Unlimited Content',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: 4,
+        imgSrc: Setting,
+        altText: 'Customization',
+        title: 'Unlimited Customization',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: 5,
+        imgSrc: Design,
+        altText: 'Productivity',
+        title: 'Boost Productivity',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: 6,
+        imgSrc: Chat,
+        altText: 'Support',
+        title: 'Customer Support',
+        text: 'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
 ];
 
 export default function Feature() {
-  return (
-    <section sx={{ variant: 'section.feature' }}>
-      <Container>
-        <SectionHeader
-          slogan="Quality features"
-          title="Meet exciting feature of app"
-        />
+    return (
+        <section sx={{ variant: 'section.feature' }}>
+            <Container>
+                <SectionHeader
+                    slogan="Quality features"
+                    title="Meet exciting feature of app"
+                />
 
-        <Grid sx={styles.grid}>
-          {data.map((item) => (
-            <FeatureCard
-              key={item.id}
-              src={item.imgSrc}
-              alt={item.title}
-              title={item.title}
-              text={item.text}
-            />
-          ))}
-        </Grid>
-      </Container>
-    </section>
-  );
+                <Grid sx={styles.grid}>
+                    {data.map((item) => (
+                        <FeatureCard
+                            key={item.id}
+                            src={item.imgSrc}
+                            alt={item.title}
+                            title={item.title}
+                            text={item.text}
+                        />
+                    ))}
+                </Grid>
+            </Container>
+        </section>
+    );
 }
 
 const styles = {
-  grid: {
-    pt: [0, null, null, null, null, null, null, 3],
-    gridGap: [
-      '37px 0',
-      null,
-      '45px 30px',
-      null,
-      '50px 30px',
-      null,
-      null,
-      '90px 70px',
-    ],
-    width: ['100%', '80%', '100%'],
-    mx: 'auto',
-    gridTemplateColumns: [
-      'repeat(1,1fr)',
-      null,
-      'repeat(2,1fr)',
-      null,
-      'repeat(3,1fr)',
-    ],
-  },
+    grid: {
+        pt: [0, null, null, null, null, null, null, 3],
+        gridGap: [
+            '37px 0',
+            null,
+            '45px 30px',
+            null,
+            '50px 30px',
+            null,
+            null,
+            '90px 70px',
+        ],
+        width: ['100%', '80%', '100%'],
+        mx: 'auto',
+        gridTemplateColumns: [
+            'repeat(1,1fr)',
+            null,
+            'repeat(2,1fr)',
+            null,
+            'repeat(3,1fr)',
+        ],
+    },
 };
